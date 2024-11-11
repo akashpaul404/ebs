13b6a46b068f4102ebf24ae392be9fb229fbebb7
akashpaul404 <akash.paul8080@gmail.com>
2024-11-12 04:59:43 +0530

    Updates

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PrivateRoute = () => {
-  const isLoggedIn = localStorage.getItem('isLoggedIn');  // Check login status
-  return isLoggedIn === 'true' ? <Outlet /> : <Navigate to="/" />;  // If logged in, render child routes (e.g., /home), else redirect to login
-};
-
-export default PrivateRoute;
